9369c9160303490a2610a09c68f19bb33998bc22
김선주 <sseunn123@naver.com>
2022-07-14 16:35:02 +0900

    초기세팅 및 기본 레이아웃

---
+++ til-vue/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="menu">
+    <a>Home</a>
+    <a>products</a>
+    <a>About</a>
+  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price1 }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      price1: 60,
+      price2: 70,
+      products: ["역삼동원룸", "천호동원룸", "개화동원롬"],
+    };
+  },
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
