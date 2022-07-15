dbad7ecc393cec3237032907e078b48e9c948b1f
김선주 <sseunn123@naver.com>
2022-07-15 12:12:32 +0900

    반복문 onclick사용

--- til-vue/src/App.vue
+++ til-vue/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="menu">
-    <a>Home</a>
-    <a>products</a>
-    <a>About</a>
+    <a v-for="원룸 in 메뉴들" :key="원룸">{{ 원룸 }}</a>
   </div>
   <img alt="Vue logo" src="./assets/logo.png" />
   <div>
     <h4>{{ products[0] }}</h4>
     <p>{{ price1 }}만원</p>
+    <button v-on:click="신고수++">허위매물신고</button>
+    <span>신고수 : {{ 신고수 }} </span>
   </div>
   <div>
     <h4>{{ products[1] }}</h4>
@@ -26,7 +26,9 @@ export default {
     return {
       price1: 60,
       price2: 70,
-      products: ["역삼동원룸", "천호동원룸", "개화동원롬"],
+      products: ["역삼동원룸", "천호동원룸", "개화동원롬", "마포동원룸"],
+      메뉴들: ["Home", "Shop", "About"],
+      신고수: 0,
     };
   },
   components: {},
@@ -41,5 +43,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
